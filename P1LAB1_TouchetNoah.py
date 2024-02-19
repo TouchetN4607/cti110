6d22b226d88b0ebe78471ecce2f8bda404ea9d86
TouchetN4607 <160451945+TouchetN4607@users.noreply.github.com>
2024-02-18 23:13:40 -0500

    Add files via upload
    First submission.

---
+++ P1LAB1_TouchetNoah.py
@@ -0,0 +1,11 @@
+# Noah Touchet
+# 02-18-24
+# P1LAB1
+# This project shows the student can use IDLE to create a python script. 
+
+# Get user input for the first name
+first_name = input("Enter your first name: ")
+
+# Output a welcome message using the entered first name
+print(f"Hey {first_name}")
+print("Welcome to ZyBooks!")
